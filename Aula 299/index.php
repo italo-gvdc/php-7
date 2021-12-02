f396ea5b2d63456c8fe0919b1e8f2328c6989e5a
Italo <italogabrielvc@Gmail.com>
2021-12-02 10:31:10 -0300

    Embutindo blocos php em páginas HTML

---
+++ Aula 299/index.php
@@ -0,0 +1,14 @@
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Curso PHP</title>
+	</head>
+
+	<body>
+		<?php
+			echo 'Ultilizando a tag padrão';
+		?>
+		<br>
+		<?= 'Ultilizando a tag impressão' ?>
+	</body>
+</html>
