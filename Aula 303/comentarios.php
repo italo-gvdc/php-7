194ae83886c880760ef42114041e9d32d654a50b
Italo <italogabrielvc@gmail.com>
2021-12-02 12:38:34 -0300

    Comentarios

---
+++ Aula 303/comentarios.php
@@ -0,0 +1,23 @@
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Curso PHP</title>
+	</head>
+
+	<body>
+		<?php 
+
+			// Comentario de uma linha
+			echo 'Comando echo <br>';
+			
+			#Comentario de uma linha
+			echo print ("Comando print <hr>");
+
+			/*
+				Comentario de multiplas linhas.
+				Este tipo de comentario permite que varias
+				linhas sejam comentadas ao mesmo tempo.
+			*/
+		?>
+	</body>
+</html>
