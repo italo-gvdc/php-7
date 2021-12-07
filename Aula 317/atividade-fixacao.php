1f007db1bc069355e69a55b763c81d52b4267f98
Italo <italogabrielvc@gmail.com>
2021-12-07 15:00:42 -0300

    Aplicação capaz de indentificar se pode ou não doar sangue

---
+++ Aula 317/atividade-fixacao.php
@@ -0,0 +1,24 @@
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>Curso PHP</title>
+</head>
+
+<body>
+    <?php
+
+        $idade = 19;
+
+        $peso = 60;
+
+        if(($idade >= 16 && $idade <= 69) && $peso >=50) {
+            echo "Atende aos requisitos";
+
+        }else{
+            echo "Não atende aos requisitos";
+        }
+    ?> 
+</body>
+
+</html
