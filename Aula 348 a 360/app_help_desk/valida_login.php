29b655f3d292d309867c6f265c7864feac66063f
Italo <italogabrielvc@gmail.com>
2021-12-22 17:35:44 -0300

    App help desk

--- Aula 348 a 360/app_help_desk/valida_login.php
+++ Aula 348 a 360/app_help_desk/valida_login.php
@@ -1,19 +1,24 @@
 <?php
 
 session_start();
+
+/*
 $_SESSION['x'] = 'Oi, eu sou um valor de sessão!';
 print_r($_SESSION);
 echo '<hr/>';
 print_r($_SESSION['y']);
-
+*/
 
 //VARIAVEL QUE VERIFICA SE A AUTENTICAÇÃO FOI REALIZADA
 $usuario_autenticado = false;
+$usuario_id = null;
 
 //USUARIOS DO SISTEMA
 $usuarios_app = array(
-    array('email' => 'adm@teste.com.br', 'senha' => '123456'),
-    array('email' => 'user@teste.com.br', 'senha' => 'abcd')
+    array('id' => 1, 'email' => 'adm@teste.com.br', 'senha' => '1234'),
+    array('id' => 2, 'email' => 'user@teste.com.br', 'senha' => '1234'),
+    array('id' => 3, 'email' => 'jose@teste.com.br', 'senha' => '1234'),
+    array('id' => 4, 'email' => 'maria@teste.com.br', 'senha' => '1234'),
 );
 /*
 
@@ -32,6 +37,7 @@ foreach($usuarios_app as $user){
     */
     if($user['email'] == $_POST['email'] && $user['senha'] == $_POST['senha']){
         $usuario_autenticado = true;
+        $usuario_id = $user['id'];
     }
 }
 
@@ -41,10 +47,10 @@ foreach($usuarios_app as $user){
         $_SESSION['autenticado'] = 'SIM';
         $_SESSION['x'] = 'um valor';
         $_SESSION['y'] = 'outro valor';
-        header('Location: home.php');
+        //header('Location: home.php');
     }else{  
         $_SESSION['autenticado'] = 'NÃO';
-        header('Location: index.php?login=erro');
+        //header('Location: index.php?login=erro');
       }
 
 
