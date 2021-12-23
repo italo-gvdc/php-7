a63a2a7dbbf7f4db4590289393b6432b233ac7db
Italo <italogabrielvc@gmail.com>
2021-12-23 12:08:07 -0300

    Aplicando controle de perfil

--- Aula 348 a 360/app_help_desk/valida_login.php
+++ Aula 348 a 360/app_help_desk/valida_login.php
@@ -45,8 +45,7 @@ foreach($usuarios_app as $user){
         echo 'Usuário autenticado.';
 
         $_SESSION['autenticado'] = 'SIM';
-        $_SESSION['x'] = 'um valor';
-        $_SESSION['y'] = 'outro valor';
+        $_SESSION['id'] = $usuario_id;
         //header('Location: home.php');
     }else{  
         $_SESSION['autenticado'] = 'NÃO';
