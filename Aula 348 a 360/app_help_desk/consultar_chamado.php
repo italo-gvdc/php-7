cd58e68a70e89f885cdabb2067928a7792aa029f
Italo <italogabrielvc@gmail.com>
2021-12-24 13:37:43 -0300

    Desafio, app help desk

--- Aula 348 a 360/app_help_desk/consultar_chamado.php
+++ Aula 348 a 360/app_help_desk/consultar_chamado.php
@@ -1,28 +1,45 @@
-<?php 
-require_once "validador_acesso.php";
-  
-?>
+<?php require_once "validador_acesso.php" ?>
 
 <?php
 
-  //array de chamados
-
+  //chamados
   $chamados = array();
-  //http://php.net/manual/pt_BR/function.fopen.php
-  //abrir arquivo.hd
-  $arquivo = fopen('arquivo.hd','r');
 
-  //enquato houverem registros (linhas) a serem recuperados
-  while(!feof($arquivo)){ //testa pelo fim do arquivo
-    //linhas
-    $registro = fgets($arquivo);//recupera a linha
-    $chamados[] = $registro;
+  echo $_SESSION['perfil_id'];
+
+  //abrir o arquivo.hd
+  $arquivo = fopen('../../app_help_desk/arquivo.hd', 'r');
+
+  //enquanto houver registros (linhas) a serem recuperados
+  while(!feof($arquivo)) { //testa pelo fim de um arquivo
+    //linhas  
+    $registro = fgets($arquivo);
+
+    //explode dos detalhes do registro para verificar o id do usuário responsável pelo cadastro
+    $registro_detalhes = explode('#', $registro);
+
+    //(perfil id = 2) só vamos exibir o chamado, se ele foi criado pelo usuário
+    if($_SESSION['perfil_id'] == 2) {
+
+      //se usuário autenticado não for o usuário de abertura do chamado então não faz nada
+      if($_SESSION['id'] != $registro_detalhes[0]) {
+        continue; //não faz nada
+
+      } else {
+        $chamados[] = $registro; //adiciona o registro do arquivo ao array $chamados
+      }
+
+    } else {
+      $chamados[] = $registro; //adiciona o registro do arquivo ao array $chamados
+    }
+
   }
 
-  //fechando o arquivo.hd
+  //fechar o arquivo aberto
   fclose($arquivo);
-
+  
 ?>
+
 <html>
   <head>
     <meta charset="utf-8" />
@@ -48,9 +65,7 @@ require_once "validador_acesso.php";
       </a>
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a href="logoff.php" class="nav-link">
-          SAIR
-          </a>
+          <a class="nav-link" href="logoff.php">SAIR</a>
         </li>
       </ul>
     </nav>
@@ -65,37 +80,33 @@ require_once "validador_acesso.php";
             </div>
             
             <div class="card-body">
+
+              <?php foreach($chamados as $chamado) { ?>
               
-              <?php foreach($chamados as $chamado){ ?>
+                <?php
 
-              <?php
-                $chamado_dados = explode('#', $chamado);
+                  $chamado_dados = explode('#', $chamado);
 
-                if($_SESSION['perfil_id'] == 2){
-                  //apenas exibir se foi criado pelo mesmo usuario
-                  if($chamado_dados[0] != $_SESSION['id']){
+					        //não existe detalhes do chamado se ele não estiver completo
+                  if(count($chamado_dados) < 3) {
                     continue;
                   }
-                }
 
-                if(count($chamado_dados) < 3){
-                  continue;
-                }
-              ?>
-              <div class="card mb-3 bg-light">
-                <div class="card-body">
-                  <h5 class="card-title"><?= $chamado_dados[1]?></h5>
-                  <h6 class="card-subtitle mb-2 text-muted"><?= $chamado_dados[2]?></h6>
-                  <p class="card-text"><?= $chamado_dados[3]?></p>
+                ?>
+                <div class="card mb-3 bg-light">
+                  <div class="card-body">
+                    <h5 class="card-title"><?=$chamado_dados[1]?></h5>
+                    <h6 class="card-subtitle mb-2 text-muted"><?=$chamado_dados[2]?></h6>
+                    <p class="card-text"><?=$chamado_dados[3]?></p>
 
+                  </div>
                 </div>
-              </div>
 
               <?php } ?>
 
               <div class="row mt-5">
                 <div class="col-6">
-                  <a href="home.php" class="btn btn-lg btn-warning btn-block">Voltar</a>
+                  <a class="btn btn-lg btn-warning btn-block" href="home.php">Voltar</a>
                 </div>
               </div>
             </div>
