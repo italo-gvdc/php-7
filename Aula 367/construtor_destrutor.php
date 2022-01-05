d74e87128b8deac83de3b6d202577e4dcee30d6d
Italo <italogabrielvc@gmail.com>
2022-01-05 16:16:35 -0300

    Construct e desctruct

--- Aula 367/construtor_destrutor.php
+++ Aula 367/construtor_destrutor.php
@@ -4,12 +4,29 @@
 
 		public $nome = null;
 
-		function __construct() {
-			
+		function __construct($nome) {
+			echo 'Objeto iniciado';
+			$this->nome = $nome;
+		}
+
+		function __destruct() {
+			echo 'Objeto removido';
+		}
+
+		function __get($atributo) {
+			return $this->$atributo;
+		}
+
+		function correr() {
+			return $this->__get('nome') . '? esta correndo';
 		}
 	}
 
-	$pessoa = new Pessoa();
+	$pessoa = new Pessoa('Jorge');
+	echo '<br />Nome: ' . $pessoa->__get('nome');
+	echo '<br />' . $pessoa->correr();
 
-
+	echo '<br />';
+	//unset($pessoa); //proposital
+ 
 ?>
