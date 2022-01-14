0d8e08251389ec0ecc3d06e3323c76a2631a6225
Italo <italogabrielvc@gmail.com>
2022-01-14 17:40:06 -0300

    App send mail

--- Aula 378 a 387/app_send_mail/processa_envio.php
+++ Aula 378 a 387/app_send_mail/processa_envio.php
@@ -1,3 +1,29 @@
 <?php 
 
 	print_r($_POST);
+
+	class mensagem {
+		private $para = null;
+		private $assunto = null;
+		private $mensagem = null;
+
+		public function __get($atributo) {
+			return $this->$atributo;
+		}
+
+		public function __set($atributo, $valor) {
+			$this->$atributo = $valor;
+		}
+
+		public function mensagemValida() {
+			//
+		}
+	}
+
+	mensagem = new Mensagem();
+
+	$mensagem->__set('para', $_POST['para']);
+	$mensagem->__set('assunto', $_POST['assunto']);
+	$mensagem->__set('mensagem', $_POST['']);
+
+	print_r($mensagem);
