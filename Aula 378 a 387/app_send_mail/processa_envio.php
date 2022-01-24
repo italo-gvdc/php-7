fbd475502578da219303ea3821e24ffde1961af0
Italo <italogabrielvc@gmail.com>
2022-01-24 15:38:12 -0300

    App send mail, melhorando o feedback visual

--- Aula 378 a 387/app_send_mail/processa_envio.php
+++ Aula 378 a 387/app_send_mail/processa_envio.php
@@ -90,5 +90,24 @@
 	</head>
 
 	<body>
+
+		<div class="container">
+			<div class="py-3 text-center">
+				<img class="d-block mx-auto mb-2" src="logo.png" alt="" width="72" height="72">
+				<h2>Send Mail</h2>
+				<p class="lead">Seu app de envio de e-mails particular!</p>
+			</div>
+
+			<div class="row">
+				<div class="col-md-12">
+					
+					<? if($mensagem->status['codigo_status'] == 1) { ?>
+
+					<? } ?>
+
+				</div>
+			</div>
+		</div> 
+
 	</body>
 </html>
